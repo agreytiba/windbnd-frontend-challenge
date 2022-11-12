8aabb2d17ce474e80ab3e102f383e3a55928d4db
agrey tbamanya <agreytibamanya04@gmail.com>
2022-11-12 18:38:33 +0300

    second project commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,19 @@
 import StarIcon from '@mui/icons-material/Star';
-import "../styles/card.css"
+import '../styles/card.css';
 const Card = () => {
-  return (
-    <div className='card'>
-      <img src="./hotel.jpg" alt="" />
-      <section className='card-items'>
-        <button>SUPER HOT</button>
-        <p className='description'>Entire apartment.2beds</p>
-        <span><StarIcon className='star-icon'/> 4.0</span>
-      </section>
-      <h2>Stylist apartment in center of the city</h2>
-    </div>
-  )
-}
+	return (
+		<div className="card">
+			<img src="./hotel.jpg" alt="" />
+			<section className="card-items">
+				<button>SUPER HOT</button>
+				<p className="description">Entire apartment.2beds</p>
+				<span>
+					<StarIcon className="star-icon" /> 4.0
+				</span>
+			</section>
+			<h2>Stylist apartment in center of the city</h2>
+		</div>
+	);
+};
 
-export default Card
+export default Card;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,71 @@
 import SearchIcon from '@mui/icons-material/Search';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
-import "../styles/navbar.css"
+import '../styles/navbar.css';
 import { useState } from 'react';
 
 const Navbar = () => {
-  const [searchCity, setSearchCity] = useState(false)
-  const handleCity = () => {
-    setSearchCity(!searchCity)
-  }
-  return (
-      <div className="nav-container">
-          <img src="/logo.svg" alt="windbnb logo" />
-      <section className="search">
-        <div>
-          <input type="text" placeholder='Helsinki, Finland' className='city' onClick={handleCity}/>
-          { searchCity && <section className='city-popup'>
-           
-            <ul type="none">
-              <li><LocationOnIcon/>Helsinki, Finland</li>
-              <li><LocationOnIcon/>Turku, Finland</li>
-              <li><LocationOnIcon/>Oulu,Finland</li>
-              <li><LocationOnIcon/>voasa, Finland</li>
-            </ul>
-          </section>}
-        </div>
-        <div>
-          <input type="text" placeholder='Add guests' className='add-guest' />
-          <div>
-            <section className='guests-adults'>
-              <h4>Adults</h4>
-              <p>Ages 13 or above</p>
-              <button>-</button><span>o</span> <button>+</button>
-            </section>
-            <section className='guest-children'>
-              <h4>Children</h4>
-              <p>Ages 2-12above</p>
-              <button>-</button><span>o</span> <button>+</button>
-            </section>
-          </div>
-        </div>
-            
-             <span> <SearchIcon className='search-icon'/></span>
-          </section>
-            
-    </div>
-  )
-}
+	const [ searchCity, setSearchCity ] = useState(false);
+	const [ isAge, setIsAge ] = useState(false);
+	const handleAge = () => {
+		setIsAge(!isAge);
+		setSearchCity(false);
+	};
+	const handleCity = () => {
+		setSearchCity(!searchCity);
+		setIsAge();
+	};
+	return (
+		<div className="nav-container">
+			<img src="/logo.svg" alt="windbnb logo" />
+			<section className="search">
+				<div>
+					<input type="text" placeholder="Helsinki, Finland" className="city" onClick={handleCity} />
+					{searchCity && (
+						<section className="city-popup">
+							<ul type="none">
+								<li>
+									<LocationOnIcon /><span>Helsinki, Finland</span>
+								</li>
+								<li>
+									<LocationOnIcon /><span>Turku, Finland</span>
+								</li>
+								<li>
+									<LocationOnIcon /> <span>Oulu,Finland</span>
+								</li>
+								<li>
+									<LocationOnIcon /><span>voasa, Finland</span>
+								</li>
+							</ul>
+						</section>
+					)}
+				</div>
+				<div>
+					<input type="text" placeholder="Add guests" onClick={handleAge} className="add-guest" />
+					{isAge && (
+						<div className="age-range">
+							<section className="guest-type">
+								<h4>Adults</h4>
+								<p>Ages 13 or above</p>
+								<button>-</button>
+								<span>o</span> <button>+</button>
+							</section>
+							<section className="guest-type">
+								<h4>Children</h4>
+								<p>Ages 2-12 above</p>
+								<button className="select-room">-</button>
+								<span>o</span> <button>+</button>
+							</section>
+						</div>
+					)}
+				</div>
 
-export default Navbar
+				<button className="search-button">
+					{' '}
+					<SearchIcon className="search-icon" />
+				</button>
+			</section>
+		</div>
+	);
+};
+
+export default Navbar;
